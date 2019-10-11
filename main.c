d342a0b9d9e5aa708d8076eacbcac21ed3aee0ec
Alazarevkrutoy <55573419+Alazarevkrutoy@users.noreply.github.com>
2019-10-11 09:03:05 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
+float x,y,z,l,o,e,s;
+
+int main (void)
 {
-    int x, res;
-    scanf("%d",&x);
-    res=32*x*x*x*x*x*x;
-    printf("res=%d",res);
+    scanf("%f%f%f", &x, &y, &z);
+
+    l=2*cos(x-2.0/3.0);
+    o=1./2.+pow(sin(y),2);
+    e=(1+(pow(z,2)/(3-(pow(z,2)/5))));
+
+    s=l/o*e;
+
+    printf("%f",s);
+    printf("\n");
+
     return 0;
 }
