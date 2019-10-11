e3f7d7f42a64d3c9e9d53717dc8db729de786703
Alazarevkrutoy <55573419+Alazarevkrutoy@users.noreply.github.com>
2019-10-11 09:43:40 +0300

    Update YPR2.c

--- YPR2.c
+++ YPR2.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 
-float x,y,z,lul,lol,lel,s;
+float x,y,z,l,o,e,s;
 
 int main(int argc, char const *argv[])
 {
 	scanf("%f%f%f", &x, &y, &z);
 	
-	lul=2*cos(x-2.0/3.0);
-	lol=1./2.+pow(sin(y),2);
-	lel=(1+(pow(z,2)/(3-(pow(z,2)/5))));
+	l=2*cos(x-2.0/3.0);
+	o=1./2.+pow(sin(y),2);
+	e=(1+(pow(z,2)/(3-(pow(z,2)/5))));
 
 	s=lul/lol*lel;
 
